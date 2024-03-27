e3547cc063102d653d2869a3905f8097b54a0dec
하이미디어학원 <@DESKTOP-1NQ75LQ>
2024-03-27 14:24:06 +0900

    240327

--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -2,15 +2,26 @@ package com.javaex.ex06;
 
 public class CConverterApp {
     public static void main(String[] args) {
-        double dollar = 1_000_000;
-        double won = 100;
+//        double dollar = 100;
+//        double won = 1_000_000;
+//        
+//        CConverter.setRate(1341.40);	// 환욜 세팅
+//        
+//        //100만원을 달러로 출력하기
+//        System.out.printf("백만원은 %,.13f달러입니다", CConverter.toDoller(won));
+//        
+//        //100달려를 원으로 출력하기
+//        System.out.printf("\n백달러는 %,.1f원입니다", CConverter.toKWR(dollar));
+    	
+    	double dollar;
+    	double won;
+    	
+        CConverter.setRate(1341.40);	// 환욜 세팅
         
-        CConverter.setRate(1118.70);
-        
-        //100만원을 달러로 출력하기
-        System.out.printf("백만원은 %,.13f달러입니다", CConverter.toDoller(dollar));
-        
-        //100달려를 원으로 출력하기
-        System.out.printf("\n백달러는 %,.1f원입니다", CConverter.toKWR(won));
+    	dollar = CConverter.toDoller(1_000_000);
+    	System.out.printf("백만원은 %f달러입니다.", dollar);
+
+    	won = CConverter.toKWR(100);
+    	System.out.printf("\n달러는 %f달러입니다.", won);
     }
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -8,25 +8,31 @@ public class FriendApp {
         Friend[] friendArray = new Friend[3];
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("친구를 3명 등록해 주세요");
+        System.out.println("[ 친구를 3명 등록해 주세요 ]");
 
         
         //for문 시작
         //친구정보 3명 입력 로직 --> 반복문 사용
-        for(int i=0; i<friendArray.length; i++) {
-        	friendArray[i] = new Friend();
+        for(int i=0; i<3; i++) {
+        	//friendArray[i] = new Friend();
+        	Friend friend = new Friend();
         	
         	System.out.print("이름: ");
         	String name = sc.next();
-        	friendArray[i].setName(name);
+        	//friendArray[i].setName(name);
+        	friend.setName(name);
         	
         	System.out.print("핸드폰: ");
         	String hp = sc.next();
-        	friendArray[i].setHp(hp);
+        	//friendArray[i].setHp(hp);
+        	friend.setHp(hp);
         	
         	System.out.print("학교: ");
         	String school = sc.next();
-        	friendArray[i].setSchool(school);
+        	//friendArray[i].setSchool(school);
+        	friend.setSchool(school);
+        	
+        	friendArray[i]=friend;
 
         	System.out.println("-------------------------------");
         }
@@ -36,6 +42,8 @@ public class FriendApp {
         for (int i = 0; i < friendArray.length; i++) {
         	friendArray[i].showInfo();
         }
+//        for(Friend val : friendArray)
+//        	val.showInfo();
 
         sc.close();
     }
